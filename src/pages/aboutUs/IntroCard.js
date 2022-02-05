348afb46af37faf9735ba0cb0f8f1e88208d9a4d
Jagroop Singh <jagroopsingh@Jagroops-Mac-mini.local>
2022-02-05 15:29:27 -0800

    fixed aboutUs page margins

--- src/pages/aboutUs/IntroCard.js
+++ src/pages/aboutUs/IntroCard.js
@@ -7,22 +7,27 @@ function IntroCard({info}) {
   return (
     <Card className="intro-main-card">
       <Row>
-      <Col className="col-spacings">
-        <Card.Img variant="middle" src={logo} className="card-item-img"/>
-        <Card.Body>
-            <Card.Title>{info.name}</Card.Title>
-            <Card.Text>
-                {info.position}
-            </Card.Text>
-        </Card.Body>
-      </Col>
-      <Col className="col-spacings">
-        <div className="text-div">
-          <h3> </h3>
-          <h3>Name, year and major</h3>
-          <h6>couple sentences about you</h6>
-        </div>
-      </Col>
+
+        <Col xs="auto" className="col-spacings">
+          <Card.Img variant="middle" src={logo} className="card-item-img"/>
+          <Card.Body>
+              <Card.Text>
+                  {info.name}
+                  <br></br>
+                  {info.position}
+              </Card.Text>
+          </Card.Body>
+        </Col>
+
+        <Col className="col-spacings">
+          <div className="text-div">
+            <h3> </h3>
+            <h7>alksdf aks dfskdljfj sdkjf dskjfjs f. akdflsd fds.f .s fadlfjk sdfkjs dfs.f sdfl sdfklsd. sfd lskfjsdl fs.djf js. fkdslf sdjkl fs .k</h7>
+            <br></br>
+            <h7>jkdsfjklf sdl fsjd f. ds.fjskdlfjlsdf . asdklfkjl sdafkjl asjdklf ksdf dsjklf sdjkl fjsdkl jlkfsjkjkldjfkjklsdjk fjklsd fsdjkf jsdfjsd fjlsd fskdl fsdjk fsj</h7>
+          </div>
+        </Col>
+
       </Row>
     </Card>
   );
